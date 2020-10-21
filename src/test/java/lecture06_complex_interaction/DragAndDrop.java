8d62f0f13a92e5dfed9dae9301203f6bc26e96d1
Nijat Muhtar <yasunaz1990@gmail.com>
2020-10-21 19:11:39 -0400

    done

---
+++ src/test/java/lecture06_complex_interaction/DragAndDrop.java
@@ -0,0 +1,34 @@
+package lecture06_complex_interaction;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDrop {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/drag_and_drop");
+        WebElement from = driver.findElement(By.cssSelector("div#column-a"));
+        WebElement to = driver.findElement(By.cssSelector("div#column-b"));
+        pause(2);
+        Actions action = new Actions(driver);
+        pause(2);
+        action.dragAndDrop(from, to).build().perform();
+        pause(2);
+        driver.quit();
+    }
+
+    public static void pause(int second ) {
+        try{
+            Thread.sleep(second * 1000);
+        }catch (Exception e){
+
+        }
+    }
+}
